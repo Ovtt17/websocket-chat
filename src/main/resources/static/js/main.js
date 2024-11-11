677c68e7ac3e98a03845a6c78fc2ec6f89e8f44f
Ovett Mora <ovettmora17@gmail.com>
2024-11-10 18:20:52 -0600

    Add main.js to implement WebSocket chat functionality and handle user interactions

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const usernamePage = document.querySelector('#username-page');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#usernameForm');
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const messageArea = document.querySelector('#messageArea');
+const connectingElement = document.querySelector('.connecting');
+
+let stompClient = null;
+let username = null;
