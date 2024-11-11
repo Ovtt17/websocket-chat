c52207123f3fb09175229f0af6204bae8fc5ba5d
Ovett Mora <ovettmora17@gmail.com>
2024-11-10 18:21:13 -0600

    Add color palette for chat messages in main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,3 +10,8 @@ const connectingElement = document.querySelector('.connecting');
 
 let stompClient = null;
 let username = null;
+
+const colors = [
+  '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+  '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
